104e7da2dc89cd1ab6abc407d9f0e5dca61b93ce
Roman <igoshinr.estho@gmail.com>
2022-08-24 22:14:51 +0300

    66

---
+++ 66/Program.cs
@@ -0,0 +1,11 @@
+﻿int f(int m, int n)
+{
+    if (n == m)
+        return m;
+    return n + f(m, n - 1);
+}
+
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(m, n));
